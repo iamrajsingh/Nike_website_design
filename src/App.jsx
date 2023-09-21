f459fd0641ceef33b9351e1d422cc7e19575f7a2
iamrajsingh <divyraj65407@gmail.com>
2023-09-21 23:24:24 +0530

    Navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
+import Nav from "./components/nav";
+import {
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
     <section className="padding-x">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer />
     </section>
     <section className="padding bg-pale-blue">
-      CustomerReviews
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe />
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      Footer
+      <Footer />
     </section>
-
   </main>
-)
+);
 
 export default App;
