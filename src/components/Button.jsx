602cc7ffd7205cfeebf0ce67171a1f847128cde7
iamrajsingh <divyraj65407@gmail.com>
2023-09-23 23:12:54 +0530

    Footer done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,29 @@
-const Button = ({label, iconURL, backgroundColor, borderColor, textColor, fullWidth}) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullWidth,
+}) => {
   return (
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg 
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg 
     
-    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'bg-coral-red  text-white'}
+    ${
+      backgroundColor
+        ? `${backgroundColor} ${textColor} ${borderColor}`
+        : "bg-coral-red  text-white"
+    }
     
-    leading-none rounded-full ${fullWidth && 'w-full'}`}>
-        {label}
-        {iconURL && <img src={iconURL} alt={label} className="ml-2 rounded-full w-5 h-5"/> }
+    leading-none rounded-full ${fullWidth && "w-full"}`}
+    >
+      {label}
+      {iconURL && (
+        <img src={iconURL} alt={label} className="ml-2 rounded-full w-5 h-5" />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
